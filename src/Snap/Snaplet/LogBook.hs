9bae794ff24f6f4d00723b1bedbe48e19271965c
Parnell Springmeyer <parnell@ixmat.us>
2014-07-28 18:44:01 -0500

    Initial snaplet entry written based off of the Fay snaplet

--- src/Snap/Snaplet/LogBook.hs
+++ src/Snap/Snaplet/LogBook.hs
@@ -1,3 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Snap.Snaplet.LogBook where
 
-import Snap.Snaplet.LogBook.Internal
+import           Control.Applicative
+import           Control.Monad
+import           Control.Monad.Reader
+import           Control.Monad.State.Class
+import           Control.Monad.Trans.Writer
+import qualified Data.Aeson                    as A
+import qualified Data.ByteString.Char8         as BS
+import qualified Data.ByteString.Lazy          as BL
+import qualified Data.Configurator             as C
+import           Snap.Core
+import           Snap.Snaplet
+
+import           Paths_logbook
+import           Snap.Snaplet.LogBook.Internal
+
+initLogBook :: SnapletInit b LogBook
+initLogBook = makeSnaplet "logbook" description datadir $ do
+    _  <- getSnapletUserConfig
+    fp <- getSnapletFilePath
+
+    return $ LogBook fp
+
+  where
+    datadir     = Just $ liftM (++ "/resources") getDataDir
+    description = "Turnkey blogging application for the Snapframework."
--- src/Snap/Snaplet/LogBook/Internal.hs
+++ src/Snap/Snaplet/LogBook/Internal.hs
@@ -1,3 +1,11 @@
-module Snap.Snaplet.LogBook.Internal
-    (
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Snap.Snaplet.LogBook.Internal where
+
+import           System.Directory
+import           System.FilePath
+
+-- | Configuration data type.
+data LogBook = LogBook
+  { snapletFilePath :: FilePath
+  } deriving (Show)
